6b1d85fe6a2099d88b66521bb2ff6ec8902905ed
weronika0000 <weronikav00@gmail.com>
2023-06-15 14:58:26 +0200

    Initial commit

---
+++ src/main/java/com/softwaremind/todotracker/TodoTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.softwaremind.todotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softwaremind/todotracker/TodoTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softwaremind.todotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
